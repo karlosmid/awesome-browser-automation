80b05528b20ec9ab58c33ec12e183d40a67bba4f
Karlo Smid <karlo.smid@gmail.com>
2020-01-06 19:55:24 +0100

    Use Watir element locators.

--- features/step_definitions/shirt.rb
+++ features/step_definitions/shirt.rb
@@ -1,6 +1,5 @@
-When /^I select Dark Thug Blue-Navy T-Shirt$/ do
+When /^I select (.*)$/ do |name|
   on ShirtPage do |page|
-    page.add_to_cart_element.click
-    sleep 1
+    page.select(name)
   end
 end
--- features/support/shirt.rb
+++ features/support/shirt.rb
@@ -2,6 +2,8 @@ class ShirtPage
     include PageObject
     
     page_url "<%=params[:base_url]%>"
-
-    div(:add_to_cart, :xpath => '//*[@id="root"]/main/div[2]/div[3]/div[4]')
+    
+    def select name
+      element("p", {:text => name}).when_visible.siblings[1].click
+    end
 end
